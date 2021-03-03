d0b88e0c35b25f790e3f4aa1611c9f65312f3b8b
erikcorea <erikcorrea444@gmail.com>
2021-03-03 16:56:14 -0600

    setting up components

---
+++ locate/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+// import { Route, Redirect } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hi</h1>
+    </div>
+  );
+}
+
+export default App;
