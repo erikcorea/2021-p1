2bf6aa0f22b2a2a9ed1a11bc850e194bbc6dce9a
erikcorea <erikcorrea444@gmail.com>
2021-03-03 20:14:13 -0600

    Added Header Components

--- locate/src/components/UserList.js
+++ locate/src/components/UserList.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import UserItem from './UserItem';
+import Card from './Card';
 import '../styling/UserList.css';
 
 const UserList = (props) => {
     if(props.items.length === 0) {
-        return <div className="center">
-            <h2>No users found.</h2>
-        </div>
+        return (
+            <div className="center">
+                <Card>
+                    <h2>No users found.</h2>
+                </Card>
+            </div>
+        );
     }
 
     return(
