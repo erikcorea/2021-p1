acf28a371ba7156e654f6fe66b603420e6f54666
erikcorea <erikcorrea444@gmail.com>
2021-03-03 18:35:15 -0600

    set up avatar cards and added more components

--- locate/src/components/Users.js
+++ locate/src/components/Users.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import UserList from './UserList';
 
 const Users = () => {
+    const USERS = [
+			{
+				id: 'U1',
+				name: 'Erik Correa',
+				image:
+					'https://images.pexels.com/photos/257897/pexels-photo-257897.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+				places: 3,
+			},
+		];
+
     return (
-        <div>
-            <h1>Users!</h1>
-        </div>
+        <UserList items={USERS} />
     );
 };
 
